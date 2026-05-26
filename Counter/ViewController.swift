cf8649ced933a53ae873b669e0cbfa5eea68c9f8
Elizaveta <90791523+gallelizabeth@users.noreply.github.com>
2026-05-27 02:20:55 +0300

    upd

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -1,31 +1,23 @@
-//
-//  ViewController.swift
-//  Counter
-//
-//  Created by elizabeth gall on 22.05.2026.
-//
+// насчет приватности не совсем поняла, остальное вроде исправила
 
 import UIKit
 
 class ViewController: UIViewController {
     
-    
-    @IBOutlet weak var counterImgLable: UILabel!
-    
-    
-    @IBAction func clichButton(_ sender: Any) {
-        counter += 1
-        counterImgLable.text = "Значение счётчика: \(counter)"
-    }
+    @IBOutlet weak var CounterLable: UILabel!
     
     private var counter: Int = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        
-        counterImgLable.text = "Значение счётчика: \(counter)"
+        CounterLable.text = "Значение счётчика: \(counter)"
     }
+    
+    
+    @IBAction func clickButton(_ sender: Any) {
+            counter += 1
+            CounterLable.text = "Значение счётчика: \(counter)"
+        }
 
 
 }
